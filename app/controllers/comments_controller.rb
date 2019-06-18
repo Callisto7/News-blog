2d36aae53a75a1cc5fc396c33ec21dee54b0bdca
Callisto7 <gadzhievs69@gmail.com>
2019-06-18 03:23:08 +0300

    Added the ability to delete a comments, changed the style of posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,30 +1,37 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user!, only: %i(create)
-
+  before_action :authenticate_user!
+  before_action :set_comment, only: %i(destroy)
 
   def create
     @comment = Comment.new(comment_params.merge(user_id: current_user.id))
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment.post, notice: t('.notice') }
+        format.html { redirect_to @comment.post, notice: 'Комментарий успешно добавлен.' }
         format.json { render post_path(@comment.post_id), status: :created, location: @comment }
       else
-        set_post_and_comments
+        @post = @comment.post
         format.html { render  template: 'posts/show' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
 
+  def destroy
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to @comment.post, notice: 'Комментарий успешно удалён.' }
+      format.json { head :no_content }
+    end
+  end
+
   private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
 
   def comment_params
     params.fetch(:comment, {}).permit(:post_id, :text)
-  end
-
-  def set_post_and_comments
-    @post = @comment.post
-    @comments = Comment.includes(:user).all
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,6 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   validates :text, presence: true
+
+  scope :order_by_new, -> { order(created_at: :desc).includes(:user) }
 end
