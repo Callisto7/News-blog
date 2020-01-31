1f0fa8a759fb0d43f169c1c806fe46e77df3b43a
Callisto7 <gadzhievs69@gmail.com>
2020-02-01 02:30:13 +0300

    new design

--- app/models/concerns/dateable.rb
+++ app/models/concerns/dateable.rb
@@ -2,6 +2,6 @@ module Dateable
   extend ActiveSupport::Concern
 
   def created_date
-    created_at.in_time_zone(Time.zone.name).strftime('%H:%M, %d.%m.%Y')
+    created_at.in_time_zone(Time.zone.name).strftime('%d.%m.%Y')
   end
 end
